434740ba396e53e43bcf031244ee76259300a394
Thomas Ferrand <a.tom.hic3@gmail.com>
2019-09-06 17:54:09 -0400

    Clean-up

---
+++ src/script/main.ts
@@ -0,0 +1,5 @@
+import * as _ from 'lodash';
+import * as io from 'socket.io-client';
+
+
+const socket = io();
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: path.join(__dirname, 'src/script/main.ts'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'public/script')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+};
