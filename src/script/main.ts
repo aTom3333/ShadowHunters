df66e16da10e2d67a30ddbfebd8a40997e868d32
Thomas Ferrand <a.tom.hic3@gmail.com>
2019-09-08 20:33:55 -0400

    Create scaffolding for socket event handling - client-side

--- src/script/main.ts
+++ src/script/main.ts
@@ -2,6 +2,19 @@ import * as _ from 'lodash';
 import * as io from 'socket.io-client';
 import RoomApi from './client/RoomApi';
 
+async function joinRoom(room, name: string) { // TODO Temp
+    const state = await RoomApi.join(room, name);
+    // Save current Game state
+    const socket = io();
+
+    // Bind update events
+    socket.on('update:blabla', () => {});
+    socket.on('update:truc', () => {});
+
+    // Let know the server this socket is ready to be used in the correct room
+    socket.emit('request:joinroom', room, name);
+}
+
 // Temp
 (async()=>{
     const response = await RoomApi.list();
@@ -10,4 +23,3 @@ import RoomApi from './client/RoomApi';
     console.log(await RoomApi.list());
 })();
 
-const socket = io();
