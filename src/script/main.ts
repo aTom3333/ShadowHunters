0302698cbb21a491b7812529c0de907d2dfd019a
Thomas Ferrand <a.tom.hic3@gmail.com>
2019-09-12 22:28:55 -0400

    Prototype for asking the player a multiple choice question - client side

--- src/script/main.ts
+++ src/script/main.ts
@@ -1,8 +1,29 @@
 import './../style/main.scss';
 
 import * as _ from 'lodash';
+import * as $ from 'jquery';
 import * as io from 'socket.io-client';
 import RoomApi from './client/RoomApi';
+
+
+function makeAnswerChoiceRequest(socket) {
+    // TODO Put elsewhere
+    return function(data) {
+        const form = $('<form><label><span></span><select></select></label><input type="submit" value="Envoyer"/></form>');
+        form.find('span').text(data.title);
+        for(const opt of data.choices) {
+            form.find('select').append($('<option>'+opt+'</option>'));
+        }
+        form.appendTo($('body'));
+        form.on('submit', function(e) {
+            const v = $(this).find('select').val();
+            console.log(v);
+            socket.emit('response:choice', v);
+            form.remove();
+        });
+    }
+}
+
 
 async function joinRoom(room, name: string) { // TODO Temp
     const state = await RoomApi.join(room, name);
@@ -16,6 +37,8 @@ async function joinRoom(room, name: string) { // TODO Temp
     socket.on('update:playerjoined', player => { console.log(player.name + ' joined the room')});
 
     socket.on('error', payload => { console.error(payload)});
+
+    socket.on('request:choice', makeAnswerChoiceRequest(socket));
 
     // Let know the server this socket is ready to be used in the correct room
     socket.emit('request:joinroom', { room, name });
